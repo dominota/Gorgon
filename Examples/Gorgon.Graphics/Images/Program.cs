36fc635fdc38064fc7d56f9a36efc7554443faa6
Michael Winsor <github@tape-worm.net>
2019-10-23 00:15:37 -0600

    Updated Graphics.Imaging and related example to .NET Core 3.0

--- Examples/Gorgon.Graphics/Images/Program.cs
+++ Examples/Gorgon.Graphics/Images/Program.cs
@@ -42,6 +42,7 @@ namespace Gorgon.Graphics.Example
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
